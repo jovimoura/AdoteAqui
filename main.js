c93b0d59cc977b7cc301c7015c9f92acb27b9a86
jovimoura <joaovictormoura@poli.ufrj.br>
2021-09-03 11:58:59 -0300

    atualização de pagina adicionando funcoes novas

---
+++ main.js
@@ -0,0 +1,80 @@
+let adocao = document.getElementById('adocao')
+
+adocao.addEventListener('click',(event)=>{
+    event.preventDefault();
+    alert('Oi, essa página não é um site real de adoção, mas fico feliz que tenha gostado😊')
+
+});
+
+
+let contato = document.getElementById('contato')
+
+contato.addEventListener('click',(event)=>{
+    event.preventDefault();
+    alert('Oi, essa página não é um site real de adoção, mas fico feliz que tenha gostado😊')
+
+});
+
+let flavio = document.getElementById('flavio')
+
+flavio.addEventListener('click',(event)=>{
+    event.preventDefault();
+    alert('Oi, essa página não é um site real de adoção, mas fico feliz que tenha gostado😊')
+
+});
+
+let elvis = document.getElementById('elvis')
+
+elvis.addEventListener('click',(event)=>{
+    event.preventDefault();
+    alert('Oi, essa página não é um site real de adoção, mas fico feliz que tenha gostado😊')
+
+});
+
+let charllotte = document.getElementById('charllotte')
+
+charllotte.addEventListener('click',(event)=>{
+    event.preventDefault();
+    alert('Oi, essa página não é um site real de adoção, mas fico feliz que tenha gostado😊')
+
+});
+
+let tadeu = document.getElementById('tadeu')
+
+tadeu.addEventListener('click',(event)=>{
+    event.preventDefault();
+    alert('Oi, essa página não é um site real de adoção, mas fico feliz que tenha gostado😊')
+
+});
+
+let britney = document.getElementById('britney')
+
+britney.addEventListener('click',(event)=>{
+    event.preventDefault();
+    alert('Oi, essa página não é um site real de adoção, mas fico feliz que tenha gostado😊')
+
+});
+
+let sabrina = document.getElementById('sabrina')
+
+sabrina.addEventListener('click',(event)=>{
+    event.preventDefault();
+    alert('Oi, essa página não é um site real de adoção, mas fico feliz que tenha gostado😊')
+
+});
+
+let asuna = document.getElementById('asuna')
+
+asuna.addEventListener('click',(event)=>{
+    event.preventDefault();
+    alert('Oi, essa página não é um site real de adoção, mas fico feliz que tenha gostado😊')
+
+});
+
+let fushimuro = document.getElementById('fushimuro')
+
+fushimuro.addEventListener('click',(event)=>{
+    event.preventDefault();
+    alert('Oi, essa página não é um site real de adoção, mas fico feliz que tenha gostado😊')
+
+});
